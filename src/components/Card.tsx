486660b9887ad7e42f5030af889ca8af46aaa26a
Ash Johns <mail@ashpjohns.com>
2023-11-30 18:54:29 -0500

    create add to cart on product card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,10 +1,25 @@
 import Image from 'next/image'
+import { useEffect, useState } from 'react'
 
 import { urlForImage } from '~/lib/sanity.image'
 import { type Product } from '~/lib/sanity.queries'
 import styles from '~/styles/card.module.css'
 
 export default function Card({ product }: { product: Product }) {
+
+  const [inCart, setInCart] = useState(false);
+
+  const addToCart = () => {
+    localStorage.setItem(product._id, JSON.stringify(product));
+    setInCart(true);
+  }
+
+  useEffect(() => {
+    if (localStorage.getItem(product._id)) {
+      setInCart(true);
+    }
+  }, []);
+
   return (
     <div className={styles.card}>
       <div className={styles.cover}>
@@ -24,6 +39,11 @@ export default function Card({ product }: { product: Product }) {
             {/* </a> */}
           </h3>
           <p className={styles.price}>${product.price}</p>
+        </div>
+        <div className={styles.addToCart}>
+          <button onClick={addToCart} disabled={inCart}>
+            {inCart ? '✔' : 'Add to Cart'}
+          </button>
         </div>
       </div>
     </div>
